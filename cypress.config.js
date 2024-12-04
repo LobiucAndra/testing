7401fd389aaa2cdace541b0f0199b937f8981beb
LobiucAndra <andra.lobiuc@student.usv.ro>
2024-12-04 16:26:30 +0200

    Am verificat partea de login pentru toate rolurile

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "g4s3g2", // ID-ul proiectului tău
+    baseUrl: 'http://localhost:5173',
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Căutăm testele în directorul cypress/e2e
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementează event listeners dacă este necesar
     },
   },
 });
